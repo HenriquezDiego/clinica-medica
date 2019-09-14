81896afaf5b49d8bc73c594fa70eb2b8007278c2
Diego Henriquez <df.henriquez@hotmail.com>
2019-09-14 11:56:07 -0500

    Creando un punto de salida para la  biblioteca de clases .NET Standard

---
+++ src/ClinicaMedica.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ClinicaMedica.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+           
+        }
+    }
+}
