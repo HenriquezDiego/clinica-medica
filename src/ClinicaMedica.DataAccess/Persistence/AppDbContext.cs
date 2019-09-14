398e31c58db02b1f40be3fb5933df404f87c132d
Diego Henriquez <df.henriquez@hotmail.com>
2019-09-14 11:23:53 -0500

    Se ha agregado AppDbContext

---
+++ src/ClinicaMedica.DataAccess/Persistence/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ClinicaMedica.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicaMedica.DataAccess.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<Doctor> Doctores { get; set; }
+        public DbSet<DoctorEspecialidad> DoctorEspecialidad { get; set; }
+        public DbSet<Especialidad> Especialidades { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<FacturaDetalle> FacturaDetalle { get; set; }
+        public DbSet<HistorialMedico> HistorialesMedicos { get; set; }
+        public DbSet<HistorialMedicoDetalle> HistorialMedicoDetalles { get; set; }
+        public DbSet<Medicamento> Medicamentos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<UnidadMedida> UnidadesDeMedidas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "server=localhost;Database=ClinicaDbV1.0.0;Integrated Security=true;MultipleActiveResultSets=true;");
+        }
+    }
+}
